9447247b0f680e623a4c22cb404f6150cc5f2f48
swapnil-pixel <71588863+swapnil-pixel@users.noreply.github.com>
2022-06-06 20:59:26 +0530

    text_replacer function

---
+++ text_replacer.py
@@ -0,0 +1,13 @@
+def text_replacer(file_path, old_text, new_text):
+    """
+    Replaces all occurrences of old_text with new_text in input file.
+    """
+    with open(file_path, 'r') as file:
+        file_text = file.read()
+    file_text = file_text.replace(old_text, new_text)
+    with open(file_path, 'w') as file:
+        file.write(file_text)
+             
+text_replacer("example.txt", "placement", "Screening" )
+        
+
